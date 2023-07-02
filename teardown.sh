ff06aa27aab36d58e6ad4da5de77334e3d230351
tdeshields <61873965+tdeshields@users.noreply.github.com>
2023-07-02 09:48:12 -0500

    Create teardown.sh
    teardown of mailman container stack

---
+++ teardown.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+dir_one="/opt/mailman/web"
+dir_two="/opt/mailman/core"
+dir_three="/opt/mailman/databse"
+
+keepone="settings_local.py"
+keeptwo="mailman-extra.cfg"
+
+find "$dir_one" ! -name "$keepone" -type f -delete
+find "$dir_one" -type d -empty -delete
+
+find "$dir_two" ! -name "$keeptwo" -type f -delete
+find "$dir_two" -type d -empty -delete
+
+find "$dir_one" ! -name "$keepone" -type f -delete
+find "$dir_one" -type d -empty -delete
+
+find "$dir_three" -type f -delete
+find "$dir_three" -type d -empty -delete
