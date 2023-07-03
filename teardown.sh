623aedf8e183974b38c190bea0ef48518b17065a
tdeshields <61873965+tdeshields@users.noreply.github.com>
2023-07-03 09:43:28 -0500

    Update teardown.sh

--- teardown.sh
+++ teardown.sh
@@ -14,8 +14,5 @@ find "$dir_one" -type d -empty -delete
 find "$dir_two" ! -name "$keeptwo" -type f -delete
 find "$dir_two" -type d -empty -delete
 
-find "$dir_one" ! -name "$keepone" -type f -delete
-find "$dir_one" -type d -empty -delete
-
 find "$dir_three" -type f -delete
 find "$dir_three" -type d -empty -delete
